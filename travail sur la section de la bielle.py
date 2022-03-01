a351e0b2346a7884fb2616925587b800cc7369fd
valentinbassom <100703823+valentinbassom@users.noreply.github.com>
2022-03-01 19:30:58 +0100

    Update travail sur la section de la bielle.py

--- travail sur la section de la bielle.py
+++ travail sur la section de la bielle.py
@@ -19,31 +19,30 @@ R=C/2 @distance entre le maneton et le tourillon@ #en [m]
 #Mais on va simuler un cycle complet ? donc 2* 360 ?
 theta= np.arange(-180*2,180*2,5)
 V_output=np.zeros(len(theta))
-def V_cyl(theta):
+def V_cyl(Theta):
     b=L/R
     Vc=np.pi*((D)**2)*D/4
-    for index,j in enumerate(theta):
-        v=(Vc/2)*(1-cos(theta)+b-sqrt(b**2-sin(theta)**2))+(1*Vc)/(tau-1)
+    for index,j in enumerate(Theta):
+        v=(Vc/2)*(1-cos(Theta)+b-sqrt(b**2-sin(Theta)**2))+(1*Vc)/(tau-1)
         V_output[index]=v
     return V_output
 
-def derivee_Vcyl():
+def derivee_Vcyl(Theta):
     return dV_dTheta
 
-def apport_chaleur(theta): # calculer l'apport de chaleur sur la durée de temps de combustion voir son schéma 
-    return Q*(1-cos(pi*(theta-thetaC)/deltaThetaC))/2 # en [J]
+def apport_chaleur(Theta): # calculer l'apport de chaleur sur la durée de temps de combustion voir son schéma 
+    return Q*(1-cos(pi*(Theta-thetaC)/deltaThetaC))/2 # en [J]
 
-def derivee_chaleur(theta):
-    return Q*pi*sin(pi*(theta-thetaC)/deltaThetaC)/(2*deltaThetaC)
+def derivee_chaleur(Theta):
+    return Q*pi*sin(pi*(Theta-thetaC)/deltaThetaC)/(2*deltaThetaC)
 
-def derivee_pression(theta,p):
-    return ( -gamma*p*derivee_Vcyl(theta) + (gamma-1)*derivee_chaleur(theta) )/V_cycl(theta)
+def derivee_pression(Theta,p):
+    return ( -gamma*p*derivee_Vcyl(Theta) + (gamma-1)*derivee_chaleur(Theta) )/V_cycl(Theta)
 
 def force_bielle():
     return F_pied_output et F_tete_output # en [N]
 
 def pression_cylindre():
-    
     return p_output # en [Pa]
 
 def epaisseur_critique(): #? voir schema de l enonce
@@ -51,5 +50,4 @@ def epaisseur_critique(): #? voir schema de l enonce
 
 
 def myfunc(rpm, s, theta, thetaC, deltaThetaC):
-    #VOTRE CODE
-    return (V_output, Q_output, F_pied_output, F_tete_output, p_output, t)  
+    return (V_output, Q_output, F_pied_output, F_tete_output, p_output, t)  #v
