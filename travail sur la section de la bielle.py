42c4232ab7114b59971c37f3754212174ba5c411
valentinbassom <100703823+valentinbassom@users.noreply.github.com>
2022-03-01 21:38:19 +0100

    Update travail sur la section de la bielle.py

--- travail sur la section de la bielle.py
+++ travail sur la section de la bielle.py
@@ -31,34 +31,41 @@ def volume(theta):
 def derivee_volume(theta):
     return (Vc/2)*(sin(theta)+(sin(theta)*cos(theta))/(sqrt(b**2-sin(theta)**2)))
 
-def apport_chaleur(theta): # calculer l'apport de chaleur sur la durée de temps de combustion voir son schéma 
+def apport_chaleur(theta): # calculer l'apport de chaleur sur la durée de temps de combustion voir son schéma
+    if type(theta) != np.float64 and type(theta) != type(0.0) :
+        theta[(thetaC > theta) or (theta > thetaC + deltaThetaC)] = 0.0
+    elif (thetaC > theta) or (theta > thetaC + deltaThetaC) : return 0.0
     return Q*(1-cos(pi*(theta-thetaC)/deltaThetaC))/2 # en [J]
 
 def derivee_chaleur(theta):
+    if type(theta) != np.float64 and type(theta) != type(0.0) :
+        theta[(thetaC > theta) or (theta > thetaC + deltaThetaC)] = 0.0
+    elif (thetaC > theta) or (theta > thetaC + deltaThetaC) : return 0.0
     return Q*pi*sin(pi*(theta-thetaC)/deltaThetaC)/(2*deltaThetaC)
 
 def derivee_pression(Theta,p):
-    return ( -gamma*p*derivee_Vcyl(Theta) + (gamma-1)*derivee_chaleur(Theta) )/V_cycl(Theta)
+    return ( -gamma*p*derivee_volume(Theta) + (gamma-1)*derivee_chaleur(Theta) )/volume(Theta)
 
 def force_bielle():
-    return F_pied_output et F_tete_output # en [N]
+    return (F_pied_output,F_tete_output) # en [N]
 
 def pression_cylindre(theta):
-    result = solve_ivp(derivee_pression,(-pi,pi),s*10**5,t_eval = theta)
+    result = solve_ivp(derivee_pression,(-pi,pi),np.array([s*10**5]),t_eval = theta)
     #if(result.t != theta) : print("Attention aux angles évalués")
-    return result.y # en [Pa]
+    return result.y.reshape(len(result.y[0])) # en [Pa]
 
 def epaisseur_critique(): #? voir schema de l enonce
     return t  # en [m]
 
-def derivee_pression(theta,p):
-    return dp/dtheta= -gamma*(p/V)*derivee_Vcyl(theta)+ (gamma-1)*(1/V)*derivee_chaleur(theta)
 
 def myfunc(rpm, s, theta, thetaC, deltaThetaC):
     #
     #!!!!!! theta en degrés et pas en radians
+    #thetaC le décalage par rapport à zéro dans le sens négatif
     #
     theta = theta*pi/180
+    thetaC = -thetaC*pi/180
+    deltaThetaC = deltaThetaC*pi/180
     #
     #!!!!!! reconvertir le résultat final en fonction des degrés
     #
